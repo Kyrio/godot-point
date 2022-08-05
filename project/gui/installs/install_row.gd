6f9560bb1355fe09e6b0cd13186b5c11fc2ced48
Kyrio <pou003@gmail.com>
2022-08-05 20:54:34 +0200

    Open install directory on click

--- project/gui/installs/install_row.gd
+++ project/gui/installs/install_row.gd
@@ -7,10 +7,19 @@ const CONFIG_NAMES = [ "Standard", "Mono" ]
 var install
 
 
-@onready var path := %Path as Label
+@onready var location := %Location as LineEdit
 @onready var launch := %Launch as Button
 
 
 func _ready():
-    path.text = ProjectSettings.globalize_path(install.install_path)
+    location.text = ProjectSettings.globalize_path(install.install_path)
     launch.text = CONFIG_NAMES[install.module_config]
+
+
+func _on_location_gui_input(event: InputEvent):
+    var is_click = event is InputEventMouseButton   \
+        and event.button_index == MOUSE_BUTTON_LEFT \
+        and event.pressed
+
+    if is_click or event.is_action_pressed("ui_accept"):
+        OS.shell_open("file://" + ProjectSettings.globalize_path(install.install_path))
